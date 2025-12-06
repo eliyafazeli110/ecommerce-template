2c043c89ddeb450123214bdbd468bec5b6961d1a
eliyafazeli110 <eliyafazeli110@gmail.com>
2025-12-06 08:53:03 +0330

    feat(homepage): Implement full responsive design

--- vendor/swiperSlider/swiper.js
+++ vendor/swiperSlider/swiper.js
@@ -1,11 +1,62 @@
 const swiper = new Swiper(".swiper", {
-  loop: true,
+  loop: false,
   spaceBetween: 20,
+  centeredSlides: true,
+  breakpoints: {
+    250: {
+      slidesPerView: 1.1,
+      spaceBetween: 10,
+    },
+    320: {
+      slidesPerView: 1.5,
+      spaceBetween: 15,
+    },
+    640: {
+      slidesPerView: 2.5,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+      centeredSlides: false,
+    },
+    1440: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+      centeredSlides: false,
+    },
+  },
+})
+
+const testimonialsSwiper = new Swiper(".testimonials__grid", {
+  loop: false,
+  watchOverflow: true,
+
+  navigation: {
+    nextEl: ".testimonials__btn--next",
+    prevEl: ".testimonials__btn--prev",
+  },
 
   breakpoints: {
-    320: { slidesPerView: 1 },
-    640: { slidesPerView: 2 },
-    1024: { slidesPerView: 3 },
-    1440: { slidesPerView: 4 },
+    250: {
+      slidesPerView: 1.1,
+      spaceBetween: 16,
+    },
+    320: {
+      slidesPerView: 1.3,
+      spaceBetween: 24,
+    },
+    640: {
+      slidesPerView: 2.3,
+      spaceBetween: 24,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 24,
+    },
+    1440: {
+      slidesPerView: 3.3,
+      spaceBetween: 24,
+    },
   },
 })
