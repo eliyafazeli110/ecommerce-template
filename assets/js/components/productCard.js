73664ca9268bdd931650fc9d886267e670da975c
eliyafazeli110 <eliyafazeli110@gmail.com>
2025-12-18 21:01:49 +0330

    "Render product cards dynamically on homepage"

---
+++ assets/js/components/productCard.js
@@ -0,0 +1,25 @@
+import { renderStars } from "../ui/renderStars.js"
+
+export function createProductCard(product) {
+  return `
+  <a href="/productDetailPage.html?id=${product.id}" class="product-card swiper-slide">
+      <div class="product-card__image">
+        <img src=${product.images[0].url} alt="" />
+      </div>
+
+      <div class="product-card__info">
+        <h3 class="product-card__title">${product.name}</h3>
+
+        <div class="product-card__rating">
+          ${renderStars(product.rating.average)}
+          <p>${product.rating.average}</p>
+        </div>
+      </div>
+
+      <div class="product-card__price">
+        <span class="product-card__price--current">$${product.price.current}</span>
+        <span class="product-card__price--old">$${product.price.old}</span>
+      </div>
+    </a>
+  `
+}
---
+++ assets/js/ui/renderProductsSection.js
@@ -0,0 +1,31 @@
+import { createProductCard } from "../components/productCard.js"
+
+export function renderProductSections(products) {
+  const sections = document.querySelectorAll(".product-section[data-products]")
+
+  sections.forEach((section) => {
+    const type = section.dataset.products
+    const limit = Number(section.dataset.limit) || 8
+    const container = section.querySelector(".swiper-wrapper")
+
+    if (!container) return
+
+    let filteredProducts = []
+
+    switch (type) {
+      case "featured":
+        filteredProducts = products.slice(0, 6)
+        break
+
+      case "new":
+        filteredProducts = products.slice(7, 13 + limit)
+        break
+
+      case "sale":
+        filteredProducts = products.filter((p) => p.price.old)
+        break
+    }
+
+    container.innerHTML = filteredProducts.map(createProductCard).join("")
+  })
+}
